1e57d2fe3e328b7040882610490ee5641cf80d97
Radost09 <Djudji83@gmail.com>
2022-09-16 15:49:31 +0300

    100/100

---
+++ Interfaces AndAbstraction-Lab/Cars/Seat.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Cars
+{
+    using System;
+    using System.Text;
+
+    public class Seat : ICar
+    {
+        public Seat(string model, string color)
+        {
+            this.Model = model;
+            this.Color = color;
+        }
+        public string Model { get ; set ; }
+        public string Color { get ; set ; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+        public override string ToString()
+        {
+            StringBuilder output = new StringBuilder();
+            output.AppendLine($"{this.Color} {this.Model}");
+            return output.ToString().Trim();
+        }
+    }
+}
