ef8a9013b90fb315d95db22ea71ae0dbbdd171de
Radost09 <Djudji83@gmail.com>
2022-09-16 15:43:06 +0300

    100/100

---
+++ Inheritance - Exercise/NeedForSpeed/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Vehicle sportCar = new SportCar(70, 100);
+            sportCar.Drive(8);
+            Console.WriteLine(sportCar.Fuel);
+            Console.WriteLine(sportCar.FuelConsumption);
+
+            Vehicle familyCar = new FamilyCar(100, 150);
+            familyCar.Drive(10);
+            Console.WriteLine(familyCar.Fuel);
+        }
+    }
+}
---
+++ Inheritance - Exercise/Person/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age 
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                if(value < 0)
+                {
+                    this.age = 0;
+                }
+                else
+                {
+                    this.age = value;
+                }  
+            } 
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+}
